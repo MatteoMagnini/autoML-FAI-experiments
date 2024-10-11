1f9131e41deee98f8a872d61b94319fd1b388a17
Matteo Magnini <matteo.magnini@unibo.it>
2024-10-11 10:14:00 +0200

    feat: updated docker

--- run.sh
+++ run.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-# Step 1: Check if poetry is installed
-if ! command -v poetry >/dev/null 2>&1; then
-    echo "Poetry is not installed. Installing Poetry..."
-    # Install Poetry using the official method
-    curl -sSL https://install.python-poetry.org | python3 -
-
-    # Ensure poetry is available in the current shell session
-    export PATH="$HOME/.local/bin:$PATH"
-else
-    echo "Poetry is already installed."
-fi
-
-# Step 2: Install dependencies and create the poetry environment
-echo "Setting up the poetry environment..."
-poetry install
-
-# Step 3: Execute the program by finding .yml files and running the Python module
 echo "Running experiments..."
 
 find experiments/setup -name "*.yml" | \
