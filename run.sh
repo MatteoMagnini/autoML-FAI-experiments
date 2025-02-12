2265cf178b984a8dbda39c0d3ada7601aed94fc9
Matteo Magnini <matteo.magnini@unibo.it>
2025-02-12 10:30:05 +0100

    feat: run only on adult

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 echo "Running experiments..."
 
-find experiments/setup -name "*.yml" | \
+find experiments/setup -name "adult*.yml" | \
 while read filepath; do
     filename=`echo "$filepath" | sed "s:.*/::"`
     poetry run python -m experiments "$filename"
